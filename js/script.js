4333a93e81441f46d62cfcdfaed1c13932832a9a
DhwaniJTrivedi <127465429+DhwaniJTrivedi@users.noreply.github.com>
2023-08-24 11:02:36 -0400

    added popup sliding with tabs

--- js/script.js
+++ js/script.js
@@ -39,6 +39,57 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+const open_btn = document.querySelector('.open-btn');
+const close_btn = document.querySelector('.close-btn');
+const popup = document.querySelector('.popup');
+const main_popup = document.querySelector('.main-popup');
 
+open_btn.addEventListener('click', () => {
+	popup.style.display = 'flex';
+	main_popup.style.cssText = 'animation:slide-in .5s ease; animation-fill-mode: forwards;';
+});
 
+close_btn.addEventListener('click', () => {
+	main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+	setTimeout(() => {
+		popup.style.display = 'none';
+	}, 500);
+});
+
+window.addEventListener('click', (e) => {
+	if (e.target == document.querySelector('.popup-overlay')) {
+		main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+		setTimeout(() => {
+			popup.style.display = 'none';
+		}, 500);
+	}
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const openBtn = document.querySelector(".open-btn");
+  const popup = document.getElementById("popup");
+  const popupContentContainer = document.getElementById("popup-content-container");
+  const closeBtn = document.getElementById("close-btn");
+  
+  openBtn.addEventListener("click", function () {
+    // Fetch content from popup.html using AJAX or other methods
+    fetch("popup.html")
+      .then(response => response.text())
+      .then(content => {
+        popupContentContainer.innerHTML = content;
+        popup.style.display = "block";
+      })
+      .catch(error => console.error("Error fetching content:", error));
+  });
+  
+  closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
+  });
+  
+  // You can also close the popup if clicking outside of it
+  popup.addEventListener("click", function (event) {
+    if (event.target === popup) {
+      popup.style.display = "none";
+    }
+  });
+});
 
